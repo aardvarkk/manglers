7c28662d48ca6fe37bbbdd382f9dedbba2457389
Ian Clarkson <clarksonian@gmail.com>
2022-09-29 22:08:09 -0700

    Able to write a byte to the file

--- manglers.rs
+++ manglers.rs
@@ -1,3 +1,10 @@
+use std::fs::OpenOptions;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
+
 fn main() {
-  println!("Hello World!");
+  let mut file = OpenOptions::new().write(true).open("video.mp4").unwrap();
+  file.seek(SeekFrom::Start(0));
+  file.write(&[0x12]);
 }
